e795592838a8aa859961d89de7e9aa2c3e55f5b8
Payton Staub <pstaub@pstaub-lt.cl.local>
2014-12-23 17:02:46 -0500

    solved peak-element

--- peak-element.py
+++ peak-element.py
@@ -2,4 +2,22 @@ class Solution:
     # @param num, a list of integer
     # @return an integer
     def findPeakElement(self, num):
-        
+        indexOfBiggest = 0
+        biggest = None
+        for x in range(0, len(num)):
+            if (biggest is None or num[x] > biggest):
+                indexOfBiggest = x
+                biggest = num[x]
+            if (x>0 and x<(len(num)-1)):
+                if (num[x-1] < num[x] and num[x+1] < num[x]):
+                    print x
+                    return x
+        print indexOfBiggest
+        return indexOfBiggest
+
+
+s = Solution();
+s.findPeakElement([1,1,1,2,1]);
+s.findPeakElement([1]);
+s.findPeakElement([1,2]);
+s.findPeakElement([-2000, -300]);
